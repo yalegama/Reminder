b65a0239071597c3aa7528d70e9a1c364696da52
Yalegama <nadunyalegama@gmai.com>
2021-10-18 17:13:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+let reminder=document.getElementById('reminder');
+let submit=document.getElementById('submit');
+let list=document.getElementById('list');
+let reset=document.getElementById('reset');
+let topList=document.getElementById('topList');
+let bottom=document.getElementById('bottom');
+let btn=document.getElementById('btn');
+let stackArray=[];
+
+submit.addEventListener('click',submitClick);
+function submitClick(e){
+    e.preventDefault();
+if(reminder.value==''){
+    alert('Enter reminder')
+}else{
+
+    stackArray.push(reminder.value);
+    
+    let listCount=`<h1>Reminders</h1>`;
+
+    stackArray.forEach((value,index,array)=>{
+        listCount+=`<h3>(0${index+1}) ${value}</h3>`
+    })
+    list.innerHTML=listCount;
+}
+
+reminder.value="";
+    }
+
+
+
+reset.addEventListener('click',()=>{
+    location.reload();
+    submitClick();
+    
+})
+
+topList.addEventListener('click',()=>{
+    stackArray.shift();
+    let listCount=`<h1>Reminders</h1>`;
+
+    stackArray.forEach((value,index,array)=>{
+        listCount+=`<h3>(0${index+1}) ${value}</h3>`
+    })
+    list.innerHTML=listCount;
+})
+
+bottom.addEventListener('click',()=>{
+    stackArray.pop();
+    let listCount=`<h1>Reminders</h1>`;
+
+    stackArray.forEach((value,index,array)=>{
+        listCount+=`<h3>(0${index+1}) ${value}</h3>`
+    })
+    list.innerHTML=listCount;
+})
